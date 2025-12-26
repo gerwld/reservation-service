9393d17bd738b2873e10cd8bee2b4cc67acff7c7
PJaworski <pjaworski.dev@gmail.com>
2025-12-26 11:34:05 +0100

    Add controller, service

---
+++ src/main/java/com/gerwld/reservation_system/ReservationController.java
@@ -0,0 +1,21 @@
+package com.gerwld.reservation_system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping()
+    public String getReservationById() {
+        return reservationService.getReservationById();
+    }
+
+}
---
+++ src/main/java/com/gerwld/reservation_system/ReservationService.java
@@ -0,0 +1,11 @@
+package com.gerwld.reservation_system;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    public String getReservationById() {
+        return "123";
+    }
+}
